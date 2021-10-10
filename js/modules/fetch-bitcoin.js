d110551928cfa2c5784598414a9ba7cad1b8c10a
Rafael Belo <rafaelbs.dev@gmail.com>
2021-10-09 23:14:02 -0300

    fetch bitcoin refatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,14 +1,9 @@
-export default function initFetchBtc() {
-  async function fetchBtc(valor) {
-    const li = document.querySelector(".btc-preco");
-
-    try {
-      const valorBtc = await fetch(valor);
-      const btcJSON = await valorBtc.json();
-      li.innerText = (1000 / btcJSON.BRL.buy).toFixed(4);
-    } catch (f) {
-      li.innerText = "Falha na comunicação com servidores";
-    }
-  }
-  fetchBtc("https://blockchain.info/ticker");
+export default function fetchBitcoin(url, target) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((bitcoin) => {
+      const btcpreco = document.querySelector(target);
+      btcpreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
+    })
+    .catch((erro) => console.log(Error(erro)));
 }
